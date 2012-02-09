2a4129492d3dd0b064387df607ca39de24a83df2
Tucker Trainor <tucker@wingedmouse.com>
2012-02-09 09:12:38 -0500

    building up code in FileThread - incomplete

--- Phase2/FileThread.java
+++ Phase2/FileThread.java
@@ -53,6 +53,30 @@ public class FileThread extends Thread
 					}
 					output.writeObject(response);
 				}
+				if(e.getMessage().equals("CGROUP"))
+				{
+					if(e.getObjContents().size() < 2)
+					{
+						response = new Envelope("FAIL-BADCONTENTS");
+					}
+					else
+					{
+						if(e.getObjContents().get(0) == null) {
+							response = new Envelope("FAIL-BADGROUP");
+						}
+						if(e.getObjContents().get(1) == null) {
+							response = new Envelope("FAIL-BADTOKEN");
+						}
+						else {
+//							UserToken yourToken = (UserToken)e.getObjContents().get(0); //Extract token
+//							List<String> groupList = yourToken.getGroups(); // get groups
+//							response = new Envelope("OK"); //Success
+//							response.addObject(groupList);
+						}
+					}
+					output.writeObject(response);
+				}
+
 				// Handler to list files that this user is allowed to see
 				if(e.getMessage().equals("LFILES"))
 				{
