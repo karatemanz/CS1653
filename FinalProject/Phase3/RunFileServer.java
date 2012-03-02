616154cfda46c12fe8684be22a45572baeae083a
Tucker Trainor <tucker@wingedmouse.com>
2012-03-02 14:40:06 -0500

    updated args

--- FinalProject/Phase3/RunFileServer.java
+++ FinalProject/Phase3/RunFileServer.java
@@ -3,19 +3,21 @@
 public class RunFileServer {
 	
 	public static void main(String[] args) {
-		if (args.length > 0) {
+		if (args.length == 3) {
 			try {
-				FileServer server = new FileServer(Integer.parseInt(args[0]));
+				FileServer server = new FileServer(Integer.parseInt(args[0]),
+												   args[1],
+												   Integer.parseInt(args[2]));
 				server.start();
 			}
 			catch (NumberFormatException e) {
-				System.out.printf("Enter a valid port number or pass no arguments to use the default port (%d)\n", FileServer.SERVER_PORT);
+				System.out.println("Enter valid integer port numbers.");
+				System.out.println("Usage: java RunFileServer <port> <Group Server address> <Groups Server port>");
 			}
 		}
 		else {
-			FileServer server = new FileServer();
-			server.start();
+			System.out.println("Enter correct number of arguments.");
+			System.out.println("Usage: java RunFileServer <port> <Group Server address> <Group Server port>");
 		}
 	}
-
 }
